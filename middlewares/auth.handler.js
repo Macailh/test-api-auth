7877360ec5acbfd5adf71eaaa87d18373dc81749
Macailh <salvadorenriquegb@gmail.com>
2023-07-08 15:32:55 -0600

    feat: add handler for protect with api key

---
+++ middlewares/auth.handler.js
@@ -0,0 +1,14 @@
+const boom = require("@hapi/boom");
+const { config } = require("./../config/config");
+
+function checkApiKey(req, res, next) {
+  const apiKey = req.headers["api"];
+  if (apiKey === config.APIKEY) {
+    next();
+  } else {
+    const error = boom.unauthorized("API Key inválida");
+    res.status(error.output.statusCode).json(error.output.payload);
+  }
+}
+
+module.exports = { checkApiKey };
